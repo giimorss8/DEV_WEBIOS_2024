ddcc2156a3ba906b85c436a7130cec713161ea67
Giovanna Miranda <miranda.gi4@outlook.com>
2024-10-17 20:42:45 -0300

    att

---
+++ JS/JS.AulaErmerson/Metodos  17.10/array.js
@@ -0,0 +1,34 @@
+// let grupo01 = ['Baco', 4, `Jeferson`, "Sabino"]
+// //             0     1     2           3
+// console.log (grupo01 [2]);
+// console.log (typeof grupo01 [2]);
+
+// console.log (grupo01 [0]);
+// console.log (typeof grupo01[1]);
+
+//socorro****
+
+// let Carros = ['Onix', `Celta`, "Versa", ]
+// console.log (typeof grupo01[3])
+
+// console.log (Carros [3])
+
+let nome = prompt ('Qual é o seu nome?');
+
+alert (`Olá ${nome}`);
+
+// Atividade solicitada em aula:
+
+
+let idade = prompt ('Digite sua idade:');
+let resposta = idade >=18 ? "Maior de idade" : "Menor de idade"
+
+alert ('Seja bem-vindo ${nome}, sua idade é ${idade}')
+
+
+
+
+
+
+
+
