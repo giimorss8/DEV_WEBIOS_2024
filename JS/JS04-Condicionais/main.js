60f38a07932107b24d9f6b289297fa83f76d62ec
Giovanna Miranda <miranda.gi4@outlook.com>
2024-10-21 21:35:42 -0300

    condicoes

---
+++ JS/JS04-Condicionais/main.js
@@ -0,0 +1,35 @@
+let caioComBlusa = false
+
+if (caioComBlusa == true){
+console.log ("Caio pode assistir a aula!") //false
+} 
+
+//  console.clear ()
+
+// Um jogador somente irá passar para a fase 02 se atingir mil pontos. 
+//  let pontos = 1000 
+//  if (pontos >= 1000){ //true
+//  console.log ("Passou para a fase 02")
+//  } // false
+
+console.clear ()
+// Resto da divisão do número 2, por 0 então é PAR
+//Resto da divisão do número 2, por 1 então é IMPAR
+
+let imparPar = 18
+
+if (imparPar % 2 ==0){
+    console.log ("Par")
+    {Else}
+
+    console.log ("Impar")
+}
+
+// else {  //false
+// console.log ("Caio, você não vai assistir a aula, e terá uma conversa com a mãe da Gabriella")
+// }
+
+// PS:
+// = > Atribuição
+// ==> Comparação (Se ''true'' é igual (==) a true)
+/// === > 
