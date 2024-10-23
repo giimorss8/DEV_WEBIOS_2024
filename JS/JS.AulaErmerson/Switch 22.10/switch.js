29be4472a8e97954e909c310905b941cfa0401c1
Giovanna Miranda <miranda.gi4@outlook.com>
2024-10-22 21:10:38 -0300

    edits

---
+++ JS/JS.AulaErmerson/Switch 22.10/switch.js
@@ -0,0 +1,61 @@
+// let media, faltas;
+// media = 10
+// faltas = 4
+// // 6.5
+
+// if (media >= 7 && faltas <= 5){
+//     console.log(`Aluno Aprovado.`);
+// } else if (media >= 6.5 && faltas <= 5) {
+//     console.log(`Aluno Ap. Conselho.`);
+// } else {
+//     console.log(`Aluno Reprovado.`);
+// }
+// console.log(`Fim.`);
+
+//Exercício:
+
+// let imc, peso, altura;
+// altura = 1,90
+// peso = 40
+// imc = peso/(altura * altura);
+
+// if (imc <18.5) {
+//     console.log(`Seu IMC é: ${imc.toFixed(2)} e você está abaixo do peso`);
+
+// } else if (imc >= 18.5 && imc < 25) {
+//     console.log(`Seu IMC é: ${imc.toFixed(1)} e seu peso está normal`);
+
+// } else {
+//     console.log(`Seu IMC é: ${imc.toFixed(1)} e seu peso está acima do peso.`);
+// }
+
+
+let valor = 8
+
+
+switch (valor) {
+    case 1:
+        console.log(`Você foi avaliado com ótimo`);
+        break;
+        
+    case 2:
+        console.log(`Você foi avaliado com Bom`);
+        break;
+
+    case 3:
+        console.log(`Você foi avaliado com Regular`);
+        break;
+
+    case 4:
+        console.log(`Você foi avaliado com Ruim`);
+        break;
+
+    case 5:
+        console.log(`Você foi avaliado com Péssimo`);
+        break;
+
+    default:
+        console.log(`Você não foi avaliado!`)
+        break;
+
+}
---
+++ JS/JS.AulaErmerson/Switch 22.10/while.js
@@ -0,0 +1,20 @@
+
+
+
+// let contador = 0;
+
+// while (contador <= 1000) {
+// console.log(`Valor: ${contador}`);
+// contador +=100
+// }
+
+// console.log (`fim.`);
+
+let contador = 10;
+
+ while (contador >= 0) {
+    contador +=100
+ console.log(`Valor: ${contador}`);
+ 
+ }
+ console.log (`fim.`);
