e66c0a6ac91df1508fba96a2a96d03d05eca0781
Giovanna Miranda <miranda.gi4@outlook.com>
2024-10-22 21:44:17 -0300

    fibonacci

---
+++ JS/JS.AulaErmerson/Switch 22.10/for.js
@@ -0,0 +1,15 @@
+let grupoAlunos = ['Nicolas', 'Andrew', 'Pasti', 'Caio']
+
+//Declaração Var.
+//Teste lógico.
+// Incremento / Decremento
+
+for (let i =0; i <grupoAlunos.length; i++) {
+console.log(`Boa noite: ${grupoAlunos} Seja bem vindo! \n\n`);
+}
+
+//Sequência de Fibonacci
+
+let number1;
+
+
