5056a8fa80312228a196d15f37b55a6f35be2ccc
Giovanna Miranda <miranda.gi4@outlook.com>
2024-10-30 21:42:50 -0300

    array

---
+++ JS/JSS007-ArrayObj/main.js
@@ -0,0 +1,31 @@
+let aluno = {
+    nome: 'Mánassés B',
+    idade: 12,
+    altura: 1.56, //eles não trabalham com '','', apenas ''.''
+    isCracha: true,
+    isCamisa: false,
+    isPaciencia: true,
+    isChora: true,
+cor: 'bege',
+notas: [10,8,9,7,8]
+}
+
+//retornar o objeto
+console.log (aluno)
+
+//retorna apenas o nome
+console.log (aluno.nome)
+
+//retorna apenas o campo ''isChora''
+console.log (aluno.isChora)
+// console.log( `O aluno ${aluno.nome} chora demais ? \n R: ${aluno.isChora if (){}}`)
+
+for (i= 0; i < IOS.length; i++){
+    let login = ios[i]
+    if (login.isCracha = true) {
+        console.log ("O aluno tem cracha")
+        console.log (login.isCracha)
+    } else {
+        console.log ("Aluno não tem cracha")
+    }
+}
