b8173f22ccdcde993425d0e293c140f13a9c2baa
Giovanna Miranda <miranda.gi4@outlook.com>
2024-11-08 20:51:13 -0300

    arrayat

--- JS/JS09-MetodoArray/main.js
+++ JS/JS09-MetodoArray/main.js
@@ -1,20 +1,20 @@
 // Array
 
 let frutas = ["Abacaxi",
-     "Banana", 
-     "Morango", 
-     "Uva", 
-     "Limão", 
-     "Pitaya", 
-     "Maçã", 
-     "Kiwi" ]
+     "Banana",
+     "Morango",
+     "Uva",
+     "Limão",
+     "Pitaya",
+     "Maçã",
+     "Kiwi"]
 
-     frutas.forEach(Percorrer)
+frutas.forEach(Percorrer)
 
-     function Percorrer (valor, indice){
-        console.log (valor); //conteúdo da posição
-console.log (indice) // número da posição
-     }
+function Percorrer(valor, indice) {
+     console.log(valor); //conteúdo da posição
+     console.log(indice) // número da posição
+}
 
 //      for (i = 0; i <= frutas.length ; i++) {
 // console.log (frutas [i]);
@@ -29,9 +29,51 @@ let infoUsuario = [["Ana, 1346"],
 ["Caio", 566]
 ]
 
-infoUsuario.forEach ((valor, indice) => {
-     console.log(indice [1])
+infoUsuario.forEach((valor, indice) => {
+     console.log(indice[1])
 }
 
 
 )
+
+//Math.currently / Math.max / Math.min /math.sqrt
+let numeros = [4, 9, 16, 25, 78, 34]
+
+//trazer um novo array atualizado com a raiz quadrado de cada número do array
+let numerosDois = numeros.map(Math.sqrt)
+let maiorNumero = Math.max.apply
+let menorNumero = Math.min.apply(Math,)
+
+
+//console.log (numeros)
+//console.log (numerosDois)
+console.log(maiorNumero)
+
+
+//Crie um array de 8 números aleatórios > numerosTres
+
+// let filtraNumero = numeros.filter ((valor)) => {
+//      return
+// }
+
+let tarefa = [
+     {
+          tarefa: "Dormir",
+          isFeita: true
+     },
+     {
+          tarefa: "limpar",
+          isFeita: false
+     },
+     {
+
+          tarefa: "torcer",
+          isFeita: true
+     }
+]
+
+//retorne apenas as tarefas que estão marcadas como true
+//find
+tarefa.find ((item) => {
+
+})
