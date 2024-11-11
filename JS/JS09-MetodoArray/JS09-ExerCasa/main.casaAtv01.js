0cf57cbc52d5ce2bc1db7e46b185e7d58e59a0c2
Giovanna Miranda <miranda.gi4@outlook.com>
2024-11-11 20:46:23 -0300

    atv

--- JS/JS09-MetodoArray/JS09-ExerCasa/main.casaAtv01.js
+++ JS/JS09-MetodoArray/JS09-ExerCasa/main.casaAtv01.js
@@ -1,14 +1,15 @@
-// Exerc 1: Crie um Array unidimensional com 8 posições contendo os seguintes valores 17, 43, 8, 4, 97, 56, 29 em cada posição irá conter um valor do tipo number.utilizando o método forEach crie uma função que receba cada valor do array e verifique se o número é par ou ímpar e exiba o resultado no console.log
-let valores = [17, 43, 8, 4, 97, 56, 29]
-Valores.forEach(ImparPar)
+// Exerc 1: Crie um Array unidimensional com 8 posições contendo os seguintes valores 17, 43, 8, 4, 97, 56, 29 em cada posição irá conter um valor do tipo number.utilizando o método forEach crie uma função que receba cada valor do array e verifique se o número é par ou ímpar e exiba o resultado no console.log:
 
-function ImparPar(Valor) {
+let valores = [17, 43, 8, 4, 97, 56, 29]
+valores.forEach(ImparPar)
+
+function ImparPar(valor) {
 
     if (valor % 2 == 0) {
-        console.log(Valor, "Par")
+        console.log(valor, "Par")
     }
     else {
-        console.log(Valor, "Impar")
+        console.log(valor, "Impar")
     }
 }
 
