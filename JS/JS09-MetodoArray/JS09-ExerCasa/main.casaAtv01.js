fb9e04551656f2b33a0e7c65f405dce5a369f818
Giovanna Miranda <miranda.gi4@outlook.com>
2024-11-11 20:44:31 -0300

    att

--- JS/JS09-MetodoArray/JS09-ExerCasa/main.casaAtv01.js
+++ JS/JS09-MetodoArray/JS09-ExerCasa/main.casaAtv01.js
@@ -1,5 +1,16 @@
 // Exerc 1: Crie um Array unidimensional com 8 posições contendo os seguintes valores 17, 43, 8, 4, 97, 56, 29 em cada posição irá conter um valor do tipo number.utilizando o método forEach crie uma função que receba cada valor do array e verifique se o número é par ou ímpar e exiba o resultado no console.log
+let valores = [17, 43, 8, 4, 97, 56, 29]
+Valores.forEach(ImparPar)
 
-//Crie um Array unidimensional com 14 posições contendo os seguintes valores 17, 43, 8, 4, 97, 56, 29, 3,  75,  34, 88, 82, 61 em cada posição irá conter um valor do tipo number. Utilizando Arrow Function com o método filter crie uma verificação para retornar somente os valores que estão entre o número 20 e 80.
+function ImparPar(Valor) {
+
+    if (valor % 2 == 0) {
+        console.log(Valor, "Par")
+    }
+    else {
+        console.log(Valor, "Impar")
+    }
+}
+
 
 
---
+++ JS/JS09-MetodoArray/JS09-ExerCasa/maincasaAtv02.js
@@ -0,0 +1,3 @@
+//Crie um Array unidimensional com 14 posições contendo os seguintes valores 17, 43, 8, 4, 97, 56, 29, 3,  75,  34, 88, 82, 61 em cada posição irá conter um valor do tipo number. Utilizando Arrow Function com o método filter crie uma verificação para retornar somente os valores que estão entre o número 20 e 80.
+
+let valores = [17, 43, 8, 4, 97, 56, 29, 3, 75, 34, 88, 82, 61]
